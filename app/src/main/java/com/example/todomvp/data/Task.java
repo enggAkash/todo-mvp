e6c1310c0a5a645787aa8cd7249de2f9120330ea
Akash Kumar <akash@klickinvite.com>
2019-03-19 11:41:43 +0530

    adding dependencies

---
+++ app/src/main/java/com/example/todomvp/data/Task.java
@@ -0,0 +1,13 @@
+package com.example.todomvp.data;
+
+public final class Task {
+
+
+    private final String mId;
+
+    private final String mTitle;
+
+    private final String mDescription;
+
+    private final boolean mCompleted;
+}
